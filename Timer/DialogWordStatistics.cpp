fedbaf85d99ad3734895fc0ca88bd9a680d89cbb
pestOO <EliseyZamahov@gmail.com>
2013-09-22 00:18:01 +0200

    *some UI fixs

--- Timer/DialogWordStatistics.cpp
+++ Timer/DialogWordStatistics.cpp
@@ -11,6 +11,23 @@
 #include "ui_DialogWordStatistics.h"
 #include "StatisticsTableModel.h"
 #include <QAbstractButton>
+#include <QApplication>
+#include <QFont>
+
+void prepareDialog(QWidget* const w)
+    {
+    if(w->parentWidget()) {
+        const QRect& parentGeometry = w->parentWidget()->geometry();
+        w->setMinimumSize(0,0);
+        w->adjustSize();
+        const QSize minimuSize = QSize(qMax(parentGeometry.width()/4, w->width()*6/5), w->height());
+        w->setMinimumSize(minimuSize);
+        //set new size and position in cent of parent widget
+        w->setGeometry(QStyle::alignedRect(Qt::LeftToRight, Qt::AlignCenter, w->size(), parentGeometry));
+        }
+    else
+        qWarning() << QLatin1Literal("no parent widget!");
+    }
 
 DialogWordStatistics::DialogWordStatistics(const WordRepeater& wordList, QWidget *parent) :
     QDialog(parent),
@@ -18,13 +35,21 @@ DialogWordStatistics::DialogWordStatistics(const WordRepeater& wordList, QWidget
     {
     ui->setupUi(this);
     ui->buttonBox->addButton (tr("Ok"), QDialogButtonBox::RejectRole);
-    ui->tableView->setModel( new StatisticsTableModel(wordList));
 
     QAbstractButton* cornerButton = ui->tableView->findChild<QAbstractButton*>();
     Q_ASSERT(cornerButton);
     cornerButton->setEnabled(false);
     cornerButton->setText(tr(" Round "));
     cornerButton->installEventFilter(this);
+
+    ui->tableView->setModel( new StatisticsTableModel(wordList));
+    ui->tableView->clearSelection ();
+
+    QFont appFont = qApp->font();
+    appFont.setFamily ("Monospace");
+    ui->tableView->setFont (appFont);
+
+    prepareDialog (this);
     }
 DialogWordStatistics::~DialogWordStatistics()
     {
